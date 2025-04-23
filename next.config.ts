55127cc7c52d8469118ebfe59f55585426d451c8
Aslbek <aslbekyuldoshev767@gmail.com>
2025-04-23 11:03:34 +0500

    next config changed

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,25 @@
 /** @type {import('next').NextConfig} */
 
-import {NextConfig} from 'next';
-import createNextIntlPlugin from 'next-intl/plugin';
+// import {NextConfig} from 'next';
+// import createNextIntlPlugin from 'next-intl/plugin';
  
 
-const nextConfig: NextConfig = {};
+// const nextConfig: NextConfig = {};
  
+
+// const withNextIntl = createNextIntlPlugin();
+// export default withNextIntl(nextConfig);
+
+/** @type {import('next').NextConfig} */
+
+import { NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const nextConfig: NextConfig = {
+  output: 'standalone',
+};
 
 const withNextIntl = createNextIntlPlugin();
+
 export default withNextIntl(nextConfig);
+
